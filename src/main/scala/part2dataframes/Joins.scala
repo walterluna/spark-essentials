daad8dbe0e3c14c63a2be62e3b352e2825a1518a
Walter Luna <mywalter97@gmail.com>
2022-12-20 06:55:46 -0600

    Joins exercises

--- src/main/scala/part2dataframes/Joins.scala
+++ src/main/scala/part2dataframes/Joins.scala
@@ -1,6 +1,7 @@
 package part2dataframes
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, max}
 
 object Joins extends App {
   val spark = SparkSession.builder()
@@ -65,5 +66,52 @@ object Joins extends App {
     joinCondition,
     "left_anti"
   ).show()
+
+  /* EXERCISES
+  1. Show all employees and their max salaries
+  2. Show all employees who were never managers
+  3. Find the job titles of the best paid 10 employees
+   */
+  def readTable(tableName: String) = spark.read
+    .format("jdbc")
+    .option("driver", "org.postgresql.Driver")
+    .option("url", "jdbc:postgresql://localhost:5432/rtjvm")
+    .option("user", "docker")
+    .option("password", "docker")
+    .option("dbtable", s"public.$tableName")
+    .load()
+
+  val employeesDF = readTable("employees")
+  val salariesDF = readTable("salaries")
+  val deptManagersDF = readTable("dept_manager")
+  val titlesDF = readTable("titles")
+
+  //  1
+  val maxSalariesPerEmployeeDF = salariesDF
+    .groupBy("emp_no")
+    .agg(max("salary").as("max_salary"))
+  val employeesSalariesDF = employeesDF
+    .join(maxSalariesPerEmployeeDF, "emp_no")
+  employeesSalariesDF.show()
+
+  //  2 (anti join)
+  val empNeverManagersDF = employeesDF.join(
+    deptManagersDF,
+    employeesDF.col("emp_no") === deptManagersDF.col("emp_no"),
+    "left_anti"
+  )
+  empNeverManagersDF.show()
+
+  //  3
+  val mostRecentJobTitlesDF = titlesDF
+    .groupBy("emp_no", "title")
+    .agg(max("to_date"))
+  val bestPaidEmployeesDF = employeesSalariesDF
+    .orderBy(col("max_salary").desc)
+    .limit(10)
+  val bestPaidJobsDF = bestPaidEmployeesDF
+    .join(mostRecentJobTitlesDF, "emp_no")
+  bestPaidJobsDF.show()
+
 
 }
